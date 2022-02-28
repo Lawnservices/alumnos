96c8054d6ddc9f3f41057288b20fa8f92b7469f5
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-02-28 10:49:30 -0500

    good

---
+++ app.js
@@ -0,0 +1,10 @@
+function over(obj) {
+    obj.innerHTML = "Aprender es Mejor Futuro Para Todos."
+    obj.style.color = "violet"
+
+}
+
+function out(obj) {
+    obj.innerHTML = "Lista de Alumnos."
+    obj.style.color = "#fff"
+}
